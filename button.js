5e2136611b31e6f274192439aa33c917b3ff69de
divya sable <divyasable21@navgurukul.org>
2023-07-29 16:03:45 +0530

    colorbutton  project is done

---
+++ button.js
@@ -0,0 +1,17 @@
+const box= document.getElementById("row");
+const color_button1 = document.getElementById("color_button1");
+color_button1.onclick=function(){
+    box.style.backgroundColor="Blue";
+};
+
+const color_button2 = document.getElementById("color_button2");
+color_button2.onclick=function(){
+    box.style.backgroundColor="green";
+};
+
+const color_button3 = document.getElementById("color_button3");
+color_button3.onclick=function(){
+    box.style.backgroundColor="yellow";
+};
+
+
